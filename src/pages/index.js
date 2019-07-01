3e63e8a5736dc905f295d0cbf5287993a7f33098
Ana Trajkovska <ana.trajkovska2015@gmail.com>
2019-07-01 21:51:13 +0200

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Image from '../components/image'
 const IndexPage = () => (
   <Layout>
     <h1>
-      <marquee direction="right">Hello from Gatsby on Now 2.0 test</marquee>
+      <marquee direction="right">Hello from Gatsby on Now 2.0 monkey</marquee>
     </h1>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
